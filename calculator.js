bb5cb8157ee55a623431c175077ec5cd7a0c2d4a
kathykate <kathy0kate@gmail.com>
2016-01-26 10:15:27 +0100

    Bg responsivness

--- calculator.js
+++ calculator.js
@@ -18,73 +18,63 @@ function clear() {
   n1 = []; n2 = []; op = null; solved = false;
 }
 function update() {
+  ir = 1366/914;
   w = window.innerWidth;
   h = window.innerHeight;
-    if (w > h) {
-      $(".blur").css("background-size", w+'px '+'auto');
+  sr = w/h;
+    if (ir < sr) {
+   $(".blur").css("background-size", w+'px '+'auto');
     } else {
-      $(".blur").css("background-size", 'auto '+h+'px');
+   $(".blur").css("background-size", 'auto '+h+'px');
     }
-  }
-
-function number(no){
-  if (solved) {
-    clear();
-    $(".top-row").html("");
-    $(".bottom-row").html("0");
-  }
-  if (!op) {
-    n1.push(no);
-  } else {
-    n2.push(no);
-  }
-  $(".top-row").append(no);
-}
-
-function operator(operator){
-  if (solved) {
-    $(".top-row").html(res);
-    clear();
-    n1.push(res);
-  }
-  if (n2.length === 0){
-    $(".top-row").html(n1);
-  } else {
-    if (op) {
-      res = result(n1, n2, op);
-      n1 = []; n2 = [];
-      n1.push(res);
-    }      
-  }
-  op = operator;
-  $(".top-row").append(op);
-}
-function equal(){
-  solved = true;
-  res = result(n1, n2, op);
-  $(".bottom-row").html(res);
-}
-
-$(document).ready(function() {
-  update();
-  window.addEventListener('resize', update);
-  
   var blur = document.getElementById("blur");
   x = blur.getBoundingClientRect().left;
   y = blur.getBoundingClientRect().top;
   $(".blur").css("background-position", -x+'px '+ -y+'px');
-  
+  }
+
+$(document).ready(function() {
+  update();
+  window.addEventListener('resize', update);
+    
   clear();
   $(".num").click(function() {
-    number($(this).html());
+    if (solved) {
+      clear();
+      $(".top-row").html("");
+      $(".bottom-row").html("0");
+    }
+    if (!op) {
+      n1.push($(this).html());
+    } else {
+      n2.push($(this).html());
+    }
+    $(".top-row").append($(this).html());
   });
 
   $(".op").click(function() {
-    operator($(this).html());
+    if (solved) {
+      $(".top-row").html(res);
+      clear();
+      n1.push(res);
+    }
+    if (n2.length === 0){
+      $(".top-row").html(n1);
+    } else {
+      if (op) {
+        res = result(n1, n2, op);
+        n1 = []; n2 = [];
+        n1.push(res);
+      }      
+    }
+    op = $(this).html();
+    $(".top-row").append(op);
   });
   
   $(".eql").click(function() {
-    equal();
+    solved = true;
+    res = result(n1, n2, op);
+    $(".bottom-row").html(res);
   });
 
   $(".cl").click(function() {
@@ -92,31 +82,5 @@ $(document).ready(function() {
     $(".top-row").html("");
     $(".bottom-row").html("0");
   });
-
-  $("body").keypress(function(e){
-    //$(".top-row").html(e.key);
-    switch (e.key){
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-      case '0':
-      case '.':
-        number(e.key);
-        break;
-      case '+':
-      case '-':
-      case '/':
-      case '*':
-        operator(e.key);
-        break;
-      case 'Enter':
-        equal();
-    }
-    });
 });
+
